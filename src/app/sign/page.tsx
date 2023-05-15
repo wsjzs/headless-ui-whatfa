b65ec324428bda9f8403f7d1510f21e7f7770938
mark <wsjzs@hotmail.com>
2023-05-15 20:05:15 +0800

    feat: sign up

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -91,7 +91,7 @@ const Sign: FC = () => {
         <p className="mt-10 text-center text-sm text-gray-500">
           Not a member?{' '}
           <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Start a 14 day free trial
+            Sign up
           </a>
         </p>
       </div>
